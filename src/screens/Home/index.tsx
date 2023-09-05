064acecd6df17da7ea8279871881e417c3195836
Ewerson Castelo Silva <ewerson.castelo@gmail.com>
2023-09-05 17:50:05 -0400

    chore: create Participant component

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,4 +1,5 @@
 import { Text, TextInput, TouchableOpacity, View } from 'react-native';
+import { Participant } from '../components/Participant';
 import { styles } from './styles';
 
 export function Home() {
@@ -32,6 +33,10 @@ export function Home() {
           <Text style={styles.buttonText}>+</Text>
         </TouchableOpacity>
       </View>
+
+      <Participant />
+      <Participant />
+      <Participant />
     </View>
   );
 }
