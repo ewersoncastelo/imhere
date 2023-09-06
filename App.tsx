c513f7dc0f1b79ceb8bd8d96c84bb7b95a39dcfc
Ewerson Castelo Silva <ewerson.castelo@gmail.com>
2023-09-06 10:38:43 -0400

    chore: changed text the statusBar to white

--- App.tsx
+++ App.tsx
@@ -1,7 +1,15 @@
+import { Fragment } from 'react';
+import { StatusBar } from 'react-native';
 import { Home } from './src/screens/Home';
 
 export default function App() {
   return (
-    <Home />
+    <Fragment>
+      <StatusBar 
+        barStyle="light-content"
+        translucent
+      />
+      <Home />    
+    </Fragment>
   );
 }
